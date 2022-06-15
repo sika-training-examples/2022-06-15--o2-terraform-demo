a574e4ef35dfe43eea614923e8d1fe608460e7ec
Ondrej Sika <ondrej@ondrejsika.com>
2022-06-15 10:42:27 +0200

    feat: Add SSH key

--- main.tf
+++ main.tf
@@ -6,3 +6,10 @@ resource "azurerm_resource_group" "training" {
   name     = "training-ondrejsika"
   location = local.LOCATION
 }
+
+resource "azurerm_ssh_public_key" "default" {
+  name                = "ondrejsika"
+  resource_group_name = azurerm_resource_group.training.name
+  location            = azurerm_resource_group.training.location
+  public_key          = file("./ssh-keys/ondrejsika.pub")
+}
